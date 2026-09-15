1d2b736744c09c3f2e114612a6dcc49eaa222984
rgracia02 <r.gracia02@ufromail.cl>
2026-09-15 09:43:28 -0300

    Update cache version in service worker

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,7 @@
   La version es el hash del sitio: cada publicacion crea un cache nuevo y los
   viejos se borran solos.
 */
-const CACHE = 'stickiq-25f2f3d4c205'
+const CACHE = 'stickiq-d6a78c61ad18'
 const PIEZAS = [
   './',
   './index.html',
